f3bcc8101b4202144045cca61b70ca5d4a67ce3c
Emre Sokullu <emre@groups-inc.com>
2018-01-31 10:29:35 +0300

    controllers populated

--- src/GraphPress/Controllers/MessagingController.php
+++ src/GraphPress/Controllers/MessagingController.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of the Pho package.
  *
@@ -7,7 +8,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace GraphPress\Controllers;
+
+ namespace GraphPress\Controllers;
 
 use CapMousse\ReactRestify\Http\Request;
 use CapMousse\ReactRestify\Http\Response;
