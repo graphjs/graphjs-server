4552cc3f3020a7d053f330adee30b7ce77cb317d
Emre Sokullu <esokullu@gmail.com>
2018-03-13 02:39:48 -0700

    AccessControlAllowOrigin

--- run.php
+++ run.php
@@ -3,5 +3,6 @@ ob_start();
 require "vendor/autoload.php";
 $server = new \GraphJS\Daemon();
 $server->setPort(1338); 
+$server->setAccessControlAllowOrigin("*");
 ob_end_flush();
 $server->serve();
