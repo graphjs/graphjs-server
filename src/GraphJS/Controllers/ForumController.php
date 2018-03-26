69186e78348bc7f16c01845b1fe38121c9d83380
Groups <esokullu@gmail.com>
2018-03-26 12:59:18 +0300

    bugfix

--- src/GraphJS/Controllers/ForumController.php
+++ src/GraphJS/Controllers/ForumController.php
@@ -186,7 +186,7 @@ class ForumController extends AbstractController
             "title" => $thread->getTitle(),
             "messages" => array_merge(
                 [[
-                    "author" => (string) ($thread->getAuthors()[0])->id(),
+                    "author" => (string) $thread->edges()->in()->current()->getAuthor()->id(),
                     "content" => $thread->getContent()
                 ]],
                 array_map(
