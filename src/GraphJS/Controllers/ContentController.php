982d633f375ce0d33cbb0c3ce70390b036bded7b
Emre Sokullu <esokullu@gmail.com>
2018-05-30 15:15:54 -0700

    fixed title matcher to be non-greedy

--- src/GraphJS/Controllers/ContentController.php
+++ src/GraphJS/Controllers/ContentController.php
@@ -71,7 +71,7 @@ class ContentController extends AbstractController
             $str = file_get_contents($url);
             if(strlen($str)>0){
               $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
-              preg_match("/\<title\>(.*)\<\/title\>/i",$str,$title); // ignore case
+              preg_match("/\<title\>(.*?)\<\/title\>/i",$str,$title); // ignore case
               return html_entity_decode($title[1]);
             }
         };
