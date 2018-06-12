ab4157670f6f6d3dfdc6881850ac8ce5742af1aa
Emre Sokullu <esokullu@gmail.com>
2018-06-11 18:49:21 -0700

    Create multitenant.php

---
+++ scripts/multitenant.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Pho package.
+ *
+ * (c) Emre Sokullu <emre@phonetworks.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
+ /**
+ * Multitenant functions are listed in this class
+ * 
+ * @author Emre Sokullu <emre@phonetworks.org>
+ */
+ final class MT {
+ 
+  public static function genEnvFile() {
+  
+  }
+  
+  public static function setupNginxConf() {
+  
+  }
+  
+  public static function reloadServers() {
+  
+  }
+  
+  public static function setupSupervisorConf() {
+  
+  }
+ 
+ }
