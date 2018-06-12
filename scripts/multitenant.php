05d938b3e915c8be50ed6232df1bf8e870cbe105
Groups <esokullu@gmail.com>
2018-06-11 19:42:20 -0700

    improvements with multitenant.php

--- scripts/multitenant.php
+++ scripts/multitenant.php
@@ -10,13 +10,31 @@
  */
  
  /**
- * Multitenant functions are listed in this class
+ * This class contains functions needed to generate a 
+ * new GraphJS tenant on a typical:
+ * * Ubuntu 16.04 Linux OS
+ * * PHP 7.1
+ * * Nginx (as a proxy server)
+ * * docker with redis and neo4j images 
+ * system.
  * 
  * @author Emre Sokullu <emre@phonetworks.org>
  */
  final class MT {
+
+     private $root;
+
+     public function __construct() {
+         $this->root = dirname(__DIR__ . "/../envs");
+     }
+
+     public function findConfigNum() {
+         $list = array_diff(scandir($this->root), array(".", ".."));
+         rsort($list, SORT_NUMERIC);
+         return (int) $list[0];
+     }
  
-  public static function genEnvFile() {
+  public static function makeEnvFile() {
   
   }
   
