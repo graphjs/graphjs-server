117e2f978d3aadf126e3de8b833dd226ebbd1bc4
Groups <esokullu@gmail.com>
2018-09-07 00:36:59 -0700

    heroku finetuning

--- run.php
+++ run.php
@@ -8,7 +8,8 @@ $cli = new Cli();
 $cli->description('GraphJS Server')
     ->opt('conf:C', 'Config file.', false)
     ->opt('port:P', 'Port number to serve requests.', false, 'integer')
-    ->opt('domain:D', 'CORS domain(s) to serve. Separate by semicolon, if multiple.', false);
+    ->opt('domain:D', 'CORS domain(s) to serve. Separate by semicolon, if multiple.', false)
+    ->opt('heroku:H', 'Set "yes" if this is a Heroku installation', false);
 $args = $cli->parse($argv, true);
 $port = $args->getOpt(
     'port', 
@@ -16,7 +17,8 @@ $port = $args->getOpt(
 );
 $configs = $args->getOpt('conf', "");
 $cors = $args->getOpt('domain', "http://localhost:8080");
-$server = new \GraphJS\Daemon($configs, $cors);
+$heroku = ($args->getOpt('heroku', false) === "yes");
+$server = new \GraphJS\Daemon($configs, $cors, $heroku);
 $server->setPort($port); 
 ob_end_flush();
 error_log(sprintf("Serving through port %s with the config file %s", (string) $port, $configs));
