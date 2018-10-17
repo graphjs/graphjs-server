fce5f83e85a245a9ea80fc7bb2cf5e4e89414b20
Groups <esokullu@gmail.com>
2018-10-16 23:08:03 -0700

    default cors domain is now localhost

--- run.php
+++ run.php
@@ -16,7 +16,7 @@ $port = $args->getOpt(
     (getenv('PORT') ?  getenv('PORT') : 1338) // default
 );
 $configs = $args->getOpt('conf', "");
-$cors = $args->getOpt('domain', "http://localhost:8080");
+$cors = $args->getOpt('domain', "http://localhost");
 $heroku = ($args->getOpt('heroku', false) === "yes");
 if($heroku) {
     $cors = getenv("CORS_DOMAIN");
