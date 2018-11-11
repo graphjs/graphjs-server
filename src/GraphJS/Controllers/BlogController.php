696f547cf102824ab1f02e108fd58f0ecc55e738
Groups <esokullu@gmail.com>
2018-11-11 01:06:44 -0800

    added BlogController sample implementation

---
+++ src/GraphJS/Controllers/BlogController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Pho package.
+ *
+ * (c) Emre Sokullu <emre@phonetworks.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+ namespace GraphJS\Controllers;
+
+  /**
+ * Takes care of Blog functionality
+ * 
+ * @author Emre Sokullu <emre@phonetworks.org>
+ */
+class BlogController extends AbstractController
+{
+    // postBlog
+    // > $user->postBlog("title", "content");
+    // editBlog
+    // dleeteBlog
+}
