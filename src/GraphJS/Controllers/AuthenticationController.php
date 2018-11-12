6855a6878a62c7a876cdaf336ca5d9a6ce8b021b
Groups <esokullu@gmail.com>
2018-11-12 04:53:18 -0800

    bugfix

--- src/GraphJS/Controllers/AuthenticationController.php
+++ src/GraphJS/Controllers/AuthenticationController.php
@@ -223,7 +223,7 @@ class AuthenticationController extends AbstractController
             $this->fail($response, "Information don't match records");
             return;
         }
-        $user = $result->results()[0];
+        $user = $result->records()[0];
         $session->set($request, "id", $user["udid"]);
         $this->succeed(
             $response, [
