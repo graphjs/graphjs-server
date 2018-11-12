6271f49207a5cf43dae10c8a3842cc306da99397
Groups <esokullu@gmail.com>
2018-11-12 04:52:45 -0800

    bugfix

--- src/GraphJS/Controllers/AuthenticationController.php
+++ src/GraphJS/Controllers/AuthenticationController.php
@@ -209,7 +209,7 @@ class AuthenticationController extends AbstractController
         );
         */
         $result = $kernel->index()->client()->run(
-            "MATCH (n:user {Username: {username}, Password: {password}}) RETURN n.udid",
+            "MATCH (n:user {Username: {username}, Password: {password}}) RETURN n.udid as udid",
             [ 
                 "username" => $username,
                 "password" => md5($password)
