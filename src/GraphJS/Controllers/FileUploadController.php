3db8df60542cb47b81d71b48a81d1bf60697f77d
Emre <esokullu@gmail.com>
2019-03-24 20:13:18 +0300

    more filetypes accepted

--- src/GraphJS/Controllers/FileUploadController.php
+++ src/GraphJS/Controllers/FileUploadController.php
@@ -64,7 +64,44 @@ class FileUploadController extends AbstractController
         rewind($stream);
 
         $document = new StreamedPart($stream);
-        $allowedContentTypes = [ 'image/jpeg'=>"jpg", 'image/png'=>"png", 'image/gif'=>"gif" ];
+        $allowedContentTypes = [
+            "image/jpeg" => "jpg",
+            "image/png" => "png",
+            "image/gif" => "gif",
+            "video/mp4" => "mp4",
+            "video/mpeg" => "mpeg",
+            "video/avi" => "avi",
+            "video/flv" => "flv",
+            "video/wmv" => "wmv",
+            "application/pdf" => "pdf",
+            "application/msword" => "doc",
+            "application/msword" => "dot",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document" => "docx",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.template" => "dotx",
+            "application/vnd.ms-word.document.macroEnabled.12" => "docm",
+            "application/vnd.ms-word.template.macroEnabled.12" => "dotm",
+            "application/vnd.ms-excel" => "xls",
+            "application/vnd.ms-excel" => "xlt",
+            "application/vnd.ms-excel" => "xla",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" => "xlsx",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.template" => "xltx",
+            "application/vnd.ms-excel.sheet.macroEnabled.12" => "xlsm",
+            "application/vnd.ms-excel.template.macroEnabled.12" => "xltm",
+            "application/vnd.ms-excel.addin.macroEnabled.12" => "xlam",
+            "application/vnd.ms-excel.sheet.binary.macroEnabled.12" => "xlsb",
+            "application/vnd.ms-powerpoint" => "ppt",
+            "application/vnd.ms-powerpoint" => "pot",
+            "application/vnd.ms-powerpoint" => "pps",
+            "application/vnd.ms-powerpoint" => "ppa",
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation" => "pptx",
+            "application/vnd.openxmlformats-officedocument.presentationml.template" => "potx",
+            "application/vnd.openxmlformats-officedocument.presentationml.slideshow" => "ppsx",
+            "application/vnd.ms-powerpoint.addin.macroEnabled.12" => "ppam",
+            "application/vnd.ms-powerpoint.presentation.macroEnabled.12" => "pptm",
+            "application/vnd.ms-powerpoint.template.macroEnabled.12" => "potm",
+            "application/vnd.ms-powerpoint.slideshow.macroEnabled.12" => "ppsm",
+            "application/vnd.ms-access" => "mdb",
+        ];
         $urls = [];
         $filetypes = [];
         if ($document->isMultiPart()) {
