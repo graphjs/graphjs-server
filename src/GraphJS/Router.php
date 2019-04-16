0298007c9294128a2d9e24dc9b5e64daf3f7c845
Emre <esokullu@gmail.com>
2019-04-16 14:39:00 -0700

    added more domains for default CORS exception

--- src/GraphJS/Router.php
+++ src/GraphJS/Router.php
@@ -38,7 +38,7 @@ class Router extends \Pho\Server\Rest\Router
      */
     protected static function expandCorsUrl(string $cors): array
     {
-        $final = ["https://graphjs.com", "http://graphjs.com", "https://www.graphjs.com", "http://www.graphjs.com"];
+        $final = ["https://graphjs.com", "http://graphjs.com", "https://www.graphjs.com", "http://www.graphjs.com", "http://build.gr.ps", "https://build.gr.ps"];
         if(strpos($cors, ";")===false) {
             $urls = [0=>$cors];
         }
