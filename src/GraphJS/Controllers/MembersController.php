c1070adc384bf2a44ce9ca0d8d7423996567b6cc
Emre <esokullu@gmail.com>
2019-08-11 13:13:52 -0700

    pagination implemented in getMembers

--- src/GraphJS/Controllers/MembersController.php
+++ src/GraphJS/Controllers/MembersController.php
@@ -55,6 +55,7 @@ class MembersController extends AbstractController
                 ];
             }
         }
+        $members = $this->paginate($members, $request->getQueryParams(), 20);
         $this->succeed($response, ["members" => $members]);
     }
  
