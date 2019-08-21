b55b308c3c5803f639aa470a018ebe2e7b44f80f
Emre <esokullu@gmail.com>
2019-08-21 05:23:46 -0700

    verbose

--- src/GraphJS/Controllers/BlogController.php
+++ src/GraphJS/Controllers/BlogController.php
@@ -234,7 +234,7 @@ class BlogController extends AbstractController
         }
         error_log("about to post");
         try {
-            $blog = $i->postBlog($data["title"], $data["content"]);
+            $blog = $i->postBlog($data["title"], $data["content"], null, null);
         }
         catch (\Exception $e) {
             error_log($e->getMessage());
