b5d6cff974798169391631cd0fe2b549e5797d66
Emre <esokullu@gmail.com>
2019-09-19 16:56:27 -0700

    json check

--- src/GraphJS/Utils.php
+++ src/GraphJS/Utils.php
@@ -143,4 +143,16 @@ class Utils
         return array_unique($final);
     }
 
+    /**
+     * Checks if the string is in JSON format
+     *
+     * @param string $json
+     * @return boolean
+     */
+    public static function isJson(string $json): bool
+    {
+        \json_decode($json);
+        return (\json_last_error()===JSON_ERROR_NONE);
+    }
+
 }
