fa879615ace74c4369c912f13504ca14fadf04aa
Emre <esokullu@gmail.com>
2019-09-20 23:54:30 -0700

    bugfixes

--- src/GraphJS/Session.php
+++ src/GraphJS/Session.php
@@ -12,6 +12,7 @@ namespace GraphJS;
 
 use HansOtt\PSR7Cookies\SetCookie;
 use HansOtt\PSR7Cookies\RequestCookies;
+use HansOtt\PSR7Cookies\Signer\Key;
 use HansOtt\PSR7Cookies\Signer\Hmac\Sha256;
 use HansOtt\PSR7Cookies\Signer\Mismatch;
 use Psr\Http\Message\ResponseInterface;
@@ -23,35 +24,55 @@ class Session
 
     public static function depend(ServerRequestInterface $request): ?string 
     {
+        echo "depend cookie 1\n";
         $signer = new Sha256();
+        echo "depend cookie 2\n";
         $key = new Key(md5(getenv("SINGLE_SIGNON_TOKEN_KEY")));
+        echo "depend cookie 3\n";
         $cookies = RequestCookies::createFromRequest($request);
+        echo "depend cookie 4\n";
         if ($cookies->has(static::COOKIE)) {
             try {
-                $idSigned = $signer->verify(
-                    $cookies->get(static::COOKIE), 
+                echo "depend cookie 5\n";
+                $idSigned = $cookies->get(static::COOKIE);
+                var_dump($idSigned);
+                //echo "depend cookie 5.5: ". $idSigned."\n";
+                $id = $signer->verify(
+                    $idSigned, 
                     $key
                 );
-                return $idSigned->getValue();
+                var_dump($id);
+                echo "depend cookie 6: ". $id->getValue()."\n";
+                return $id->getValue();
             } catch (Mismatch $e) {
                 error_log("Cookie tampered");
             }
         }
+        echo "depend cookie olmadi\n";
         return null;
     }
 
     public static function begin(ResponseInterface &$response, string $id): void
     {
+        echo "beign cookie 1\n";
         $signer = new Sha256();
+        echo "beign cookie 2\n";
         $key = new Key(md5(getenv("SINGLE_SIGNON_TOKEN_KEY")));
+        echo "beign cookie 3\n";
         $cookie = SetCookie::thatStaysForever(static::COOKIE, $id, "/");
+        echo "beign cookie 4\n";
         $signedCookie = $signer->sign($cookie, $key);
-        $cookie->addToResponse($response);
+        echo "beign cookie 5\n";
+        $response = $signedCookie->addToResponse($response);
+        echo "beign cookie 6\n";
     }
 
     public static function destroy(ResponseInterface &$response): void
     {
+        echo "bitir cookie 1\n";
         $cookie = SetCookie::thatDeletesCookie(static::COOKIE, "/");
-        $cookie->addToResponse($response);
+        echo "bitir cookie 2\n";
+        $response = $cookie->addToResponse($response);
+        echo "bitir cookie 3\n";
     }
 }
